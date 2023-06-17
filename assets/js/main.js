95b09555b72301fc146eb6e5be2e94646877e78b
drisaden <drisaden@gmail.com>
2023-06-17 11:45:03 +0100

    ch-m

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,33 +77,6 @@ function isElementInViewport(element) {
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 }
-// function countUp(element) {
-//     let target = parseInt(element.getAttribute('data-target'));
-//     let count = parseInt(element.innerText);
-//     let increment = Math.ceil(target / 100);
-//     if (count < target) {
-//         element.innerText = count + increment;
-//         setTimeout(function () { countUp(element) }, 20);
-//     } else {
-//         element.innerText = target;
-//     }
-// }
-
-// function startCounters() {
-//     let counters = document.querySelectorAll('.counter');
-//     let scrollTop = window.pageYOffset + window.innerHeight;
-//     let counterSection = document.querySelector('.counter-section');
-//     if (scrollTop > counterSection.offsetTop && !counterSection.classList.contains('counted')) {
-//         counters.forEach(function (counter) {
-//             countUp(counter);
-//         });
-//         counterSection.classList.add('counted');
-//     }
-// }
-
-// window.addEventListener('scroll', function () {
-//     startCounters();
-// });
 
 const secondS = document.getElementById("second-s");
 const image = document.getElementById("image");
@@ -154,6 +127,8 @@ showSlides(currentIndex);
 }, 3000);
 }
 
+
+  
 // Display the slide with the given index
 function showSlides(index) {
 const slides = document.getElementsByClassName("demo");
@@ -176,6 +151,30 @@ showSlides(currentIndex);
 // Event listener for previous and next slide buttons
 document.addEventListener("DOMContentLoaded", function () {
 fetchImages();
+const section = document.getElementById("gallery");
+  
+// Function to check if an element is in the viewport
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to start the slideshow when section is in viewport
+function startSlideshowOnScroll() {
+  if (isElementInViewport(section)) {
+    startSlideshow();
+    window.removeEventListener("scroll", startSlideshowOnScroll);
+  }
+}
+
+// Add scroll event listener to start slideshow when section is in viewport
+window.addEventListener("scroll", startSlideshowOnScroll);
 });
 
 const form = document.querySelector('#hire-form');
